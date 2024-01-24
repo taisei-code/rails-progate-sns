82576c16bf0196f114fd17b872907b8363bee99e
Taisei <taisei96.web@gmail.com>
2024-01-25 06:42:16 +0900

    ユーザー詳細に投稿表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   validates :email, { presence: true, uniqueness: true }
   validates :password, { presence: true }
 
-  has_many :posts
+  def posts
+    return Post.where(user_id: self.id)
+  end
+  
 end

65bd816a29669aae8e62a73fb22303396e386221
Taisei <taisei96.web@gmail.com>
2024-01-25 06:51:10 +0900

    ユーザー詳細に投稿表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 before_action :authenticate_user
+before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
 
   def index
     @posts = Post.all.order(created_at: :desc)
@@ -48,6 +49,14 @@ before_action :authenticate_user
     @post.destroy
     flash[:notice] = "投稿を削除しました"
     redirect_to posts_index_path, status: :see_other
+  end
+
+   def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
   end
   
 end

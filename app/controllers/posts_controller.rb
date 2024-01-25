8e5f9252a364ec100e59640995379f61bc69e8c8
Taisei <taisei96.web@gmail.com>
2024-01-26 05:41:27 +0900

    いいね機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
   def show
     @post = Post.find_by(id: params[:id])
     @user = @post.user
+    @likes_count = Like.where(post_id: @post.id).count
   end
   
   def new
